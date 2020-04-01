8b3b72511d18aefefb4d7fcfc509dd640e228cf1
Ankur Shrivastava <ankur@ankurs.com>
2020-04-01 22:35:09 +0800

    Initial commit

---
+++ pihole_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
+
+podman stop pihole
+podman rm pihole
+podman run -d \
+    --name pihole \
+    -e TZ="Asia/Singapore" \
+    -e WEBPASSWORD="ankur" \
+    --dns=127.0.0.1 --dns=1.1.1.1 \
+    -p 443:443 \
+    -p 80:80 \
+    -p 53:53/tcp -p 53:53/udp \
+    -v "$(pwd)/etc-pihole/:/etc/pihole/:z" \
+    -v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/:z" \
+    --restart=always \
+    pihole/pihole:beta-v5.0
+podman logs -f pihole
